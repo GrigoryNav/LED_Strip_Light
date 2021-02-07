d7bf7c1e5ba4c2fb434e0e4f16eb50c9ee31ec3c
GrigoryNav <GrigoryNav@yandex.ru>
2021-02-07 18:20:01 +0300

    add BluetoothConnect class

---
+++ Android app/Led_line/app/src/main/java/com/example/led_line_app/BluetoothConnect.java
@@ -0,0 +1,61 @@
+package com.example.led_line_app;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+
+import android.os.Bundle;
+public class BluetoothConnect {
+
+    BluetoothDevice bluetoothDevice;    //hc-06
+    BluetoothAdapter btAdapter;          //BluetoothAdapter.getDefaultAdapter();
+    BluetoothSocket btSocket;
+    OutputStream outputStream;
+    String address;
+
+    public BluetoothConnect( BluetoothDevice btDevice,String address1  ){ //"98:D3:61:F9:57:97" hc-06 address
+        address=address1;
+        bluetoothDevice=btDevice;
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothDevice = btAdapter.getRemoteDevice(address);
+
+    }
+
+    public void startConnection(  ) {
+        if (bluetoothDevice != null) {
+            try {
+                Method method = bluetoothDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
+                btSocket = (BluetoothSocket) method.invoke(bluetoothDevice, 1);
+                btSocket.connect();
+
+                outputStream = btSocket.getOutputStream();
+
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void setMessage(String command) {
+        byte[] buffer = command.getBytes();
+        if( (btAdapter!=null)&&( btAdapter.isEnabled() ) ) {
+            if (outputStream != null) {
+                try {
+                    outputStream.write(buffer);
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
--- Android app/Led_line/app/src/main/java/com/example/led_line_app/MainActivity.java
+++ Android app/Led_line/app/src/main/java/com/example/led_line_app/MainActivity.java
@@ -19,76 +19,46 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     Button button3;
-
+    String address = "98:D3:61:F9:57:97";
     OutputStream outputStream ;
     BluetoothAdapter btAdapter;
     BluetoothDevice hc06;
     BluetoothSocket btSocket;
+    BluetoothConnect bluetoothConnect = new BluetoothConnect( hc06, address );
     //boolean flag=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
 
         //btAdapter = BluetoothAdapter.getDefaultAdapter();
-        hc06 = btAdapter.getRemoteDevice("98:D3:61:F9:57:97");
 
-        startConnection(hc06);
-    }
+        //btAdapter = BluetoothAdapter.getDefaultAdapter();
+        //hc06 = btAdapter.getRemoteDevice("98:D3:61:F9:57:97");
 
-
-
-    private void startConnection(BluetoothDevice device) {
-        if (device != null) {
-            try {
-                Method method = device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
-                btSocket = (BluetoothSocket) method.invoke(device, 1);
-                btSocket.connect();
-
-                outputStream = btSocket.getOutputStream();
-
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-    private void setMessage(String command) {
-        byte[] buffer = command.getBytes();
-
-        if (outputStream != null) {
-            try {
-                outputStream.write(buffer);
-                outputStream.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        bluetoothConnect.startConnection();
     }
 
     public void button1(View view){
         button1 =(Button) findViewById(R.id.button1);
         //btAdapter.getName();
-        if( (btAdapter!=null)&&( btAdapter.isEnabled() ) ){
-            setMessage("1");
-        }
+
+        bluetoothConnect.setMessage("1");
+
     }
     public void button2(View view){
         button2 =(Button) findViewById(R.id.button2);
         //btAdapter.getName();
-        if( (btAdapter!=null)&&( btAdapter.isEnabled() ) ){
-            setMessage("2");
-        }
+
+        bluetoothConnect.setMessage("2");
+
     }
     public void button3(View view){
         button3 =(Button) findViewById(R.id.button3);
         //btAdapter.getName();
-        if( (btAdapter!=null)&&( btAdapter.isEnabled() ) ){
-            setMessage("3");
-        }
+
+        bluetoothConnect.setMessage("3");
+
     }
+
 }
